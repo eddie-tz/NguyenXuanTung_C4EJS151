c172d394d4988bc87cf9da62d8f13821d333dd6f
Tùng Hoàng Minh <tugnguyen@gmail.com>
2024-05-29 20:46:06 +0700

    Update Kiem tra giua ky

---
+++ Lesson-8/BaiTap/index.js
@@ -0,0 +1,1 @@
+console.log('Cau 1');
---
+++ Lesson-8/index.js
@@ -0,0 +1,62 @@
+
+// KHOI LENH
+let soA = 6;
+
+if (soA % 2 == 0){
+    console.log('Khoi lenh 1');
+    console.log('Khoi lenh 2');
+    console.log('Khoi lenh 3');
+    console.log('Khoi lenh 4');
+} else {
+    console.log('A la so le');
+}
+
+
+
+//  CAU DIEU KIEN
+// Neu (dung) {
+//     thuc hien khoi lenh
+// } THI {
+//     thuc hien khoi lenh nay
+// }
+
+// if (dieu kien) {
+//     // KHOI LENH
+// } else {
+//     // KHOI LENH
+// }
+
+
+ let a = 0
+if (a % 2 || 0) {
+    console.log('A la so chan')
+} else {
+    console.log ('A la so le')
+}
+
+
+// // SWTICH - CASE
+
+// let day;
+// console.log(new Date().getDay())
+
+// switch (new Date().getDay())
+
+// //  VONG LAP for & while
+// //  VONG LAP FOR
+
+// let sum = 0;
+// for (i = 0; i <10; i ++; ) {
+//     sum += i
+// }
+
+console.log(sum);
+
+// VONG LAP WHILE
+sum = 0;
+let i = 0;
+while (i < 10) {
+    sum = sum + i;
+    console.log(i);
+    i++;
+}
