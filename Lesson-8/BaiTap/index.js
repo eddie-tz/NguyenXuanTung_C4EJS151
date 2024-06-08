d5152450d1209ecaff3132161e6f4955e9fa6a14
Tùng Hoàng Minh <tugnguyen@gmail.com>
2024-06-08 21:09:10 +0700

    update homepage

--- Lesson-8/BaiTap/index.js
+++ Lesson-8/BaiTap/index.js
@@ -1,1 +1,18 @@
+// CÂU 1:
 console.log('Cau 1');
+let totalSum = 0;
+
+for (let i = 1; i <= 100; i++) {
+    totalSum += i;
+}
+
+console.log("Tổng các số từ 1 đến 100 là:", totalSum);
+
+//CÂU 2:
+console.log('Câu 2');
+
+for (let i = 0; i <= 100; i++) {
+    if (i % 3 ==0 && i % 5 ==0) {
+        console.log(i);
+    }
+}
---
+++ Lesson-9/index.js
@@ -0,0 +1,12 @@
+console.log('Lesson 9');
+
+let car1 = 'Saab';
+let car2 = 'Volvo';
+let car3 = 'BMW';
+
+const cars = ['Saab', 'Volvo', 'BMW'];
+
+console.log(cars);
+
+console.log(cars[0]);
+console.log(cars[cars.length - 1]);
